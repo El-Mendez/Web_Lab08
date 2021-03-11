58db75b2457f91d6c7b5a75e47608e665724a6fb
Pablo Méndez <pablomendez778@gmail.com>
2021-03-11 01:14:21 -0600

    Por lo menos la carta ya gira...

--- app/src/components/Card/Card.js
+++ app/src/components/Card/Card.js
@@ -3,10 +3,25 @@ import "./Card.css"
 import ReactCardFlip from "react-card-flip";
 
 export default class Carta extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            wasGuessed: this.props.wasGuessed,
+            beingCompared: this.props.beingCompared,
+        }
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(e) {
+        e.preventDefault();
+        this.setState(prevState => ({ beingCompared: !prevState.beingCompared }));
+        this.props.selectCard();
+    }
+
     render() {
         return (
-            <div className={"card"} onClick={this.props.selectCard}>
-                <ReactCardFlip isFlipped={this.props.beingCompared || this.props.wasGuessed}>
+            <div className={"card"} onClick={this.handleClick}>
+                <ReactCardFlip isFlipped={this.state.beingCompared || this.state.wasGuessed}>
                     <div className={"cardFront"}>
                         Hola
                     </div>
